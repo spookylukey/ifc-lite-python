a9dd0ba04bf2e61e3bce53e3f0a7239f97feaa33
timdwebb <tdwebb7417@gmail.com>
2026-08-03 15:07:25 +0100

    Simplify tessellation quality test

--- tests/test_model.py
+++ tests/test_model.py
@@ -337,16 +337,12 @@ def test_load_both_false_still_has_metadata(medium_ifc_path: Path) -> None:
 
 
 def test_tessellation_quality(l_bar_ifc_path: Path) -> None:
-    vertex_counts = []
-    for quality in range(5):
-        tessellation_quality = TessellationQuality(quality)
+    """Applies increasing tessellation quality levels and counts the vertices for each."""
+    prev_vertex_count = 0
+    for tessellation_quality in TessellationQuality:
         model = ifc_lite.process_file(
             l_bar_ifc_path, load_geometry=True, tessellation_quality=tessellation_quality
         )
-        vertex_counts.append(model.meshes[0].vertex_count)
-
-    prev_vertex_count = None
-    for vertex_count in vertex_counts:
-        if prev_vertex_count is not None:
-            assert vertex_count > prev_vertex_count
+        vertex_count = model.meshes[0].vertex_count
+        assert vertex_count > prev_vertex_count
         prev_vertex_count = vertex_count
